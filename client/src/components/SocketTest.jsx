0057dad0e79b40f16cff8b111da45fd1168f3f43
Seo JeongWoo <frirgfgg96@naver.com>
2026-01-20 14:14:29 +0900

    fixed

--- client/src/components/SocketTest.jsx
+++ client/src/components/SocketTest.jsx
@@ -8,7 +8,7 @@ const SocketTest = () => {
 
   useEffect(() => {
     // 1. 연결할 주소 설정 (http:// 입니다! ws:// 아님)
-    const socket = new SockJS('http://localhost:8080/ws');
+    const socket = new SockJS('wss://i14c203.p.ssafy.io/ws');
     const client = Stomp.over(socket);
 
     // 2. 연결 시도
